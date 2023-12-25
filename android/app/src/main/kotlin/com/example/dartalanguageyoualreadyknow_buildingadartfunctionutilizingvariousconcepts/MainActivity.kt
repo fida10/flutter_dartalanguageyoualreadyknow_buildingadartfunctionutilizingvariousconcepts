370f5f519846bb322fd68f1faab2a107232b570f
lfenix_dart_flutter <lfenix_dart_flutter@lfenix-youtubes-Mac-mini.local>
2023-12-25 07:36:05 -0600

    first commit, fixed test cases and added to correct package

---
+++ android/app/src/main/kotlin/com/example/dartalanguageyoualreadyknow_buildingadartfunctionutilizingvariousconcepts/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dartalanguageyoualreadyknow_buildingadartfunctionutilizingvariousconcepts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
